576cf975d54ea2f374586fc5d38c7416f6539dc1
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-04-08 02:49:28 +0700

    refactor: adjust code styles

--- models/Payment.js
+++ models/Payment.js
@@ -1,37 +1,37 @@
 const mongoose = require("mongoose");
 
 const PaymentSchema = new mongoose.Schema(
-    {
-        company: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Company",
-            required: [true , "Please add a company"]
-        },
-        totalPrice: {
-            type: Number,
-            required: [true, "Please add a total price"],
-        },
-        status: {
-            type: String,
-            enum: ["initiated","authorized" ,"captured", "cancelled","failed"],
-            default: "initiated",
-        },
-        dateList: {
-            type: [Date],
-            required: [true, "Please add a list of dates"],
-        },
-        events: [
-            {
-                type: { type: String },
-                payload: { type: Object },
-                createdAt: { type: Date, default: Date.now }
-            }
-        ],
-    },
-    {
+	{
+		company: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Company",
+			required: [true, "Please add a company"],
+		},
+		totalPrice: {
+			type: Number,
+			required: [true, "Please add a total price"],
+		},
+		status: {
+			type: String,
+			enum: ["initiated", "authorized", "captured", "cancelled", "failed"],
+			default: "initiated",
+		},
+		dateList: {
+			type: [Date],
+			required: [true, "Please add a list of dates"],
+		},
+		events: [
+			{
+				type: { type: String },
+				payload: { type: Object },
+				createdAt: { type: Date, default: Date.now },
+			},
+		],
+	},
+	{
 		toJSON: { virtuals: true },
 		toObject: { virtuals: true },
-        timestamp: true,
+		timestamp: true,
 	},
 );
 
