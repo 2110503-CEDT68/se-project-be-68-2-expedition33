18f0ae32ba37703f065bd2ecf6142886a08da436
Kanit <stu33074@gmail.com>
2026-04-10 00:28:58 +0700

    for pasit

--- controllers/payments.js
+++ controllers/payments.js
@@ -242,6 +242,6 @@ exports.deletePayment = async (req, res) => {
 		res.status(200).json({ success: true, data: {} });
 	} catch (err) {
 		res.status(500).json({ success: false, msg: "Cannot delete Payment" });
-		console.log(err);
+		console.log(err); // FOR PASIT
 	}
 };
